804b733286eadff18a90cf1404e910c65e70a8c7
y0za <yoza.is12s@gmail.com>
2018-05-07 02:54:59 +0900

    Change IndexPage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+export default class IndexPage extends React.Component {
+  render() {
+    const episodes = this.props.data.allMarkdownRemark.edges
 
-export default IndexPage
+    return (
+      <div>
+        {episodes.map(({ node }) => {
+          return (
+            <div key={node.fields.slug}>
+              <h3>
+                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                  {node.frontmatter.title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "YYYY-MM-DD")
+          }
+        }
+      }
+    }
+  }
+`
