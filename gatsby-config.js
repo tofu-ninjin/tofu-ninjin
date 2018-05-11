9267ab143703493635ea1bbbdd6365078f974a79
y0za <yoza.is12s@gmail.com>
2018-05-11 20:32:36 +0900

    Set gatsby-plugin-feed to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,65 @@ module.exports = {
         name: 'pages',
       },
     },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }`,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  enclosure: {
+                    url: edge.node.frontmatter.audio.url
+                  },
+                  custom_elements: [
+                    { 'content:encoded': edge.node.html }
+                  ]
+                });
+              });
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] }
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                      audio {
+                        url
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: '/feed.xml',
+          },
+        ],
+      },
+    },
     'gatsby-transformer-remark',
     'gatsby-plugin-react-helmet'
   ],
