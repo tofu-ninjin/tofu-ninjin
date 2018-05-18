ad334cabc56e02f28256994e6dbdd68f773973f5
y0za <yoza.is12s@gmail.com>
2018-05-19 03:28:31 +0900

    Set head description content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,8 +10,7 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
@@ -39,6 +38,7 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
       }
     }
   }
