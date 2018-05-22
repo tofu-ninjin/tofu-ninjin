125d9e903f3a4a2265bef6710ed73c69b944bfaa
y0za <yoza.is12s@gmail.com>
2018-05-22 14:07:31 +0900

    Change episode template

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Card, Header } from 'semantic-ui-react'
 import AudioPlayer from '../components/audio-player'
 
 export default class EpisodeTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     return (
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <AudioPlayer src={post.frontmatter.audio.url}></AudioPlayer>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
+      <Card fluid>
+        <Card.Content>
+          <h1>{post.frontmatter.title}</h1>
+          <AudioPlayer src={post.frontmatter.audio.url}></AudioPlayer>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </Card.Content>
+      </Card>
     )
   }
 }
