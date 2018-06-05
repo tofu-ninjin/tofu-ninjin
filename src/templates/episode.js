c5e80c3ff909bc916ab2353ed00fec80bf19ac0b
y0za <yoza.is12s@gmail.com>
2018-06-05 15:38:57 +0900

    Add subtitle to episode template

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Card, Header } from 'semantic-ui-react'
 import AudioPlayer from '../components/audio-player'
+import { generateSubtitle } from '../helpers/episode'
 
 export default class EpisodeTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
+    const subtitle = generateSubtitle(post.frontmatter.topics, post.frontmatter.speakers)
+
     return (
       <Card fluid>
         <Card.Content>
           <h1>{post.frontmatter.title}</h1>
+          <p>{subtitle}</p>
           <AudioPlayer src={post.frontmatter.audio.url}></AudioPlayer>
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
         </Card.Content>
@@ -23,8 +27,13 @@ export const query = graphql`
       html
       frontmatter {
         title
+        topics
         audio {
           url
+        }
+        speakers {
+          id
+          name
         }
       }
     }
