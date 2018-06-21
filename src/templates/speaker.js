625536711823023be8b3c880e1e6b673653ac5d6
y0za <yoza.is12s@gmail.com>
2018-06-21 14:49:53 +0900

    Set title to episode and speaker page

--- src/templates/speaker.js
+++ src/templates/speaker.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import { Container, Header, Icon, Image, List, Segment } from 'semantic-ui-react'
 import styles from './speaker.module.scss'
@@ -15,33 +16,36 @@ export default class SpeakerTemplate extends React.Component {
     })
 
     return (
-      <Segment>
-        <Image src={speaker.imageUrl} size='small' centered circular></Image>
-        <Header as='h2' textAlign='center' className={styles.header}>
-          <Header.Content>
-            <span className={styles.headerName}>{speaker.name}</span>
-            <a href={`https://github.com/${speaker.github}`} target='_blank'>
-              <Icon name='github'></Icon>
-            </a>
-            <a href={`https://twitter.com/${speaker.twitter}`} target='_blank'>
-              <Icon name='twitter'></Icon>
-            </a>
-          </Header.Content>
-        </Header>
+      <div>
+        <Helmet title={`${speaker.name} | 出演者`} />
+        <Segment>
+          <Image src={speaker.imageUrl} size='small' centered circular></Image>
+          <Header as='h2' textAlign='center' className={styles.header}>
+            <Header.Content>
+              <span className={styles.headerName}>{speaker.name}</span>
+              <a href={`https://github.com/${speaker.github}`} target='_blank'>
+                <Icon name='github'></Icon>
+              </a>
+              <a href={`https://twitter.com/${speaker.twitter}`} target='_blank'>
+                <Icon name='twitter'></Icon>
+              </a>
+            </Header.Content>
+          </Header>
 
-        <div className={styles.episodeList}>
-          <Header as='h2'>出演一覧</Header>
-          <List size='big'>
-            {episodes.map((episode) => {
-              return (
-                <List.Item key={episode.fields.slug}>
-                  <Link to={episode.fields.slug}>{episode.frontmatter.title}</Link>
-                </List.Item>
-              )
-            })}
-          </List>
-        </div>
-      </Segment>
+          <div className={styles.episodeList}>
+            <Header as='h2'>出演一覧</Header>
+            <List size='big'>
+              {episodes.map((episode) => {
+                return (
+                  <List.Item key={episode.fields.slug}>
+                    <Link to={episode.fields.slug}>{episode.frontmatter.title}</Link>
+                  </List.Item>
+                )
+              })}
+            </List>
+          </div>
+        </Segment>
+      </div>
     )
   }
 }
